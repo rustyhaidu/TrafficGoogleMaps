43358e7b7cbc21f2bf7d73c9249f9ee0cbcd0cbb
Claudiu Haidu <claudiu.haidu@p3-holding.com>
2021-03-20 17:57:34 +0200

    adauga semafoare si un if pentru matching

--- app/src/main/java/com/example/intersectiesemaforizata/uberanimation/MapsActivity.java
+++ app/src/main/java/com/example/intersectiesemaforizata/uberanimation/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.example.intersectiesemaforizata.uberanimation;
 
+import android.Manifest;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,13 +15,17 @@ import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.DrawableRes;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.intersectiesemaforizata.R;
+import com.example.intersectiesemaforizata.backup.Semafor;
+import com.example.intersectiesemaforizata.backup.Spital;
 import com.example.intersectiesemaforizata.uberanimation.remote.IGoogleApi;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -43,6 +49,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -65,8 +72,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private PolylineOptions polylineOptions, blackPolylineOptions;
     private Polyline blackPolyline, greyPolyline;
 
-    private LatLng myLocation;
+    private Spital spitalAleator;
+    // private TextView spitalAlesTv;
+    List<Semafor> semafoare = new ArrayList<>();
 
+    // Unused objects
+    private LatLng mDestination;
+    private LatLng myLocation;
+    // ---------
     IGoogleApi mService;
 
 
@@ -105,6 +118,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
         LatLng bucharest = new LatLng(44.4444282, 26.1279446);
+
+        // ------------ Add Markers from backup package ---------------
+        List<Spital> spitale = new ArrayList<>();
+        spitale.add(new Spital(new LatLng(44.4417171, 26.172688), "Spital Pantelimon"));
+        spitale.add(new Spital(new LatLng(44.4389115, 26.1468962), "Spitalul Monza"));
+        spitale.add(new Spital(new LatLng(44.4409689, 26.1193474), "Spitalul Foisorul de Foc"));
+        spitale.add(new Spital(new LatLng(44.4367611, 26.0703311), "Spitalul Universitar"));
+        spitale.add(new Spital(new LatLng(44.3906953, 26.143549), "Spitalul Sf. Ioan"));
+
+        Random random = new Random();
+        int spitalAleatorNr = random.nextInt(spitale.size());
+
+        Semafor semafor = new Semafor(new LatLng(44.386393, 26.139455), "Strada Vitan-Barzesti", 1);
+        semafoare.add(semafor);
+        semafoare.add(new Semafor(new LatLng(44.385642, 26.139563), "Strada Sergent Ion", 1));
+        semafoare.add(new Semafor(new LatLng(44.385826, 26.139058), "Strada Sergent Ion", 2));
+        semafoare.add(new Semafor(new LatLng(44.403278, 26.142435), "Splaiul Unirii", 1));
+        semafoare.add(new Semafor(new LatLng(44.403934, 26.142426), "Splaiul Unirii", 2));
+
+        //mDestination = new LatLng(44.4538018, 26.1006797);
+        // spitalAleator = spitale.get(spitalAleatorNr);
+        spitalAleator = spitale.get(4);
+        mDestination = spitalAleator.getCoordonate();
+
+        // spitalAlesTv = findViewById(R.id.spitalAles);
+        // spitalAlesTv.setText(spitalAleator.getNumeSpital());
+
+        // Spital Exact - Sfantul Ioan
+        // spitalAlesTv.setText(spitale.get(4).getNumeSpital());
+
+        // Google map setup 1.0
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // Show marker on the screen and adjust the zoom level
+        //map.addMarker(new MarkerOptions().position(mOrigin).title("Origin"));
+        mMap.addMarker(new MarkerOptions().position(mDestination).title(spitalAleator.getNumeSpital()));
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(mOrigin, 8f));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(semafoare.get(0).getCoordonite())
+                .title("Semafor Vitan")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(semafoare.get(1).getCoordonite())
+                .title("Strada Sergent Ion 1")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(semafoare.get(2).getCoordonite())
+                .title("Strada Sergent Ion 2")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(semafoare.get(3).getCoordonite())
+                .title("Splaiul Unirii 1")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic)));
+
+        mMap.addMarker(new MarkerOptions()
+                .position(semafoare.get(4).getCoordonite())
+                .title("Splaiul Unirii 2")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_traffic)));
+
+
+        // -----------------------------
+
         mMap.addMarker(new MarkerOptions().position(bucharest).title("Marker in Bucharest"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(bucharest));
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
@@ -120,7 +205,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     "mode=driving&" +
                     "transit_routing_preference=less_driving&" +
                     "origin=" + bucharest.latitude + "," + bucharest.longitude + "&" +
-                    "destination=" + destination + "&" +
+                    //"destination=" + destination + "&" +
+                    "destination=" + spitalAleator.getCoordonate().latitude + "," + spitalAleator.getCoordonate().longitude + "&" +
                     "key=" + getResources().getString(R.string.map_key);
             Log.d("URL", requestUrl);
             mService.getDataFromGoogleApi(requestUrl).enqueue(new Callback<String>() {
@@ -209,6 +295,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     lng = v * endPosition.longitude + (1 - v) * startPosition.longitude;
                                     lat = v * endPosition.latitude + (1 - v) * startPosition.latitude;
                                     LatLng newPos = new LatLng(lat, lng);
+
+                                    // Add Toast to see if the car is in near a Semaphor
+                                    if(semafoare.get(4).getCoordonite().latitude == lat
+                                        && semafoare.get(4).getCoordonite().longitude == lng){
+                                        Toast.makeText(MapsActivity.this, "All Lights Are Green", Toast.LENGTH_SHORT).show();
+                                    }
+
                                     marker.setPosition(newPos);
                                     marker.setAnchor(0.5f, 0.5f);
                                     marker.setRotation(getBearing(startPosition, newPos));
